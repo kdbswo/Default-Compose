8ce7c2565d661c6af01c4cadfc843ede983f722a
kdbswo <kdbswo@gmail.com>
2023-08-31 04:21:43 +0900

    TextField, OutlinedTextField Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -3,52 +3,101 @@ package com.loci.adefault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.loci.adefault.ui.theme.DefaultTheme
 
+// TextField
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+
+                MyTextField3()
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyFirst() {
+fun MyTextField1() {
+    var textState by remember { mutableStateOf("Hello") }
 
-    Column {
-
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+    TextField(
+        value = textState,
+        onValueChange = {
+            textState = it
+        },
+        label = {
+            Text(text = "입력하는 공간")
         }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField2() {
+    var textState by remember { mutableStateOf("Hello") }
+
+    OutlinedTextField(
+        value = textState,
+        onValueChange = {
+            textState = it
+        },
+        label = {
+            Text(text = "입력하는 공간")
+        }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField3() {
+
+    var textState by remember { mutableStateOf("") }
+
+    var enteredText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = textState,
+            onValueChange = { textState = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = {
+                enteredText = textState
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "입력하기")
+        }
+
+        Text(text = "결과값 텍스트 : $enteredText")
     }
 }
 
@@ -56,6 +105,6 @@ fun MyFirst() {
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyTextField3()
     }
 }
