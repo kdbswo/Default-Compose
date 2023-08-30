d9ebc3046582ad62e2037cfb0c363dd5d2320530
kdbswo <kdbswo@gmail.com>
2023-08-31 02:45:16 +0900

    Button Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.loci.adefault
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,31 +25,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             DefaultTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyBtn()
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
+fun MyBtn() {
 
-    Column {
+    val context = LocalContext.current
 
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
-        }
+    Button(
+        onClick = {
+            Log.d("Main", "onClick")
+            Toast.makeText(context, "클릭완료", Toast.LENGTH_LONG).show()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Blue
+        ),
+        modifier = Modifier
+            .width(200.dp)
+            .height(300.dp)
+    ) {
+        Text(
+            text = "버튼버튼 버튼버튼 버튼버튼버튼버튼버튼버튼",
+            lineHeight = 30.sp,
+            fontSize = 30.sp,
+            color = Color.Red       // 마지막으로 설정한색으로 적용됨 (46번째줄과 중복됨)
+        )
     }
 }
 
@@ -56,6 +62,6 @@ fun MyFirst() {
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyBtn()
     }
 }
