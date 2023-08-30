e8fa580f411b42cc0fd28312da63e5c84a538c2a
kdbswo <kdbswo@gmail.com>
2023-08-31 03:46:41 +0900

    column/spacer/divider Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -4,58 +4,76 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.loci.adefault.ui.theme.DefaultTheme
 
+// column
+// spacer / divider
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                ColumnTest2()
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
+fun ColumnTest1() {
+    Text(
+        text = "안녕하세요1",
+        fontSize = 30.sp
+    )
+    Text(
+        text = "반갑습니다1",
+        fontSize = 30.sp,
+        modifier = Modifier.padding(top = 50.dp)
+    )
+    Text(
+        text = "반갑습니다2",
+        fontSize = 30.sp,
+        modifier = Modifier.padding(top = 100.dp)
+    )
+}
 
-    Column {
-
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
-        }
+@Composable
+fun ColumnTest2() {
+    Column(
+        modifier = Modifier.padding(30.dp)
+    ) {
+        Text(
+            text = "안녕하세요1",
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.padding(30.dp))
+        Divider(thickness = 4.dp, color = Color.Blue)
+        Text(
+            text = "반갑습니다1",
+            fontSize = 30.sp,
+        )
+        Text(
+            text = "반갑습니다2",
+            fontSize = 30.sp,
+        )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        ColumnTest2()
     }
 }
