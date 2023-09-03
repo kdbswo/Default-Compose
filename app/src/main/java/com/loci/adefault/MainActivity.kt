caa83b26e6e4f1f3b4d615c54862baa8bfdbea3e
kdbswo <kdbswo@gmail.com>
2023-09-03 17:51:00 +0900

    WebView Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -1,61 +1,38 @@
 package com.loci.adefault
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.loci.adefault.ui.theme.DefaultTheme
 
+// WebView
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
-            }
+            MyWebView("https://www.youtube.com/")
+
         }
     }
 }
 
 @Composable
-fun MyFirst() {
-
-    Column {
-
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+fun MyWebView(url : String) {
+    AndroidView(factory = {
+        WebView(it).apply {
+            loadUrl(url)
         }
-    }
+    })
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyWebView("https://www.daum.net/")
     }
 }
