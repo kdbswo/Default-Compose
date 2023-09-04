5322df002bcf79aa54ecc604fd93a747f325e644
kdbswo <kdbswo@gmail.com>
2023-09-05 04:51:22 +0900

    Code Editing Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -4,17 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.loci.adefault.ui.theme.DefaultTheme
 
@@ -23,32 +17,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyTextArea3()
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
-
+fun MyTextArea1() {
     Column {
-
+        Text(
+            text = "안녕",
+            fontSize = 100.sp,
+            color = Color.Red
+        )
+        Text(
+            text = "나는",
+            fontSize = 100.sp,
+            color = Color.Green
+        )
+        Text(
+            text = "누구",
+            fontSize = 100.sp,
+            color = Color.Blue
+        )
 
     }
+}
 
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
+@Composable
+fun MyTextArea2() {
+    Column {
+        MyTextFormat1("안녕", 100.sp, Color.Red)
+        MyTextFormat1("나는", 100.sp, Color.Green)
+        MyTextFormat1("누구", 100.sp, Color.Blue)
+    }
+}
 
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
-        }
+@Composable
+fun MyTextFormat1(text: String, fontSize: TextUnit, color: Color) {
+    Text(
+        text = text,
+        fontSize = fontSize,
+        color = color
+    )
+}
+
+@Composable
+fun MyTextArea3() {
+    MyTextFormat2 {
+        Text(
+            text = "안녕", fontSize = 100.sp, color = Color.Red
+        )
+    }
+}
+
+@Composable
+fun MyTextFormat2(content: @Composable () -> Unit) {
+    Column {
+        content()
+        content()
+        content()
+        content()
+        content()
     }
 }
 
@@ -56,6 +87,6 @@ fun MyFirst() {
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyTextArea3()
     }
 }
