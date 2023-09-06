51543ba0b7b9bbb2aa9645e47ccbdaaede4f27ce
kdbswo <kdbswo@gmail.com>
2023-09-07 03:37:13 +0900

    Show/Hide Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -3,51 +3,90 @@ package com.loci.adefault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.loci.adefault.ui.theme.DefaultTheme
 
+// show / hide
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyShowHideEx2()
             }
         }
     }
 }
 
+//버튼클릭시 새로운버튼이 나옴
 @Composable
-fun MyFirst() {
+fun MyShowHideEx1() {
+    var isButtonVisible by remember { mutableStateOf(false) }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            isButtonVisible = !isButtonVisible
+            println(isButtonVisible)
+        }) {
+            if (isButtonVisible) {
+                Text(text = "숨기기", fontSize = 50.sp)
 
+            } else {
+                Text(text = "보이기", fontSize = 50.sp)
+
+            }
+        }
+        if (isButtonVisible) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "짠짠짠", fontSize = 50.sp)
+            }
+        }
 
     }
+}
 
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
+@Composable
+fun MyShowHideEx2() {
+    var switchState by remember { mutableStateOf(false) }
 
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+    Column(
+        modifier = Modifier.padding(20.dp)
+
+    ) {
+        Switch(checked = switchState,
+            onCheckedChange = { checked ->
+                switchState = checked
+
+            }
+        )
+        Text(text = if (switchState) "ON" else "OFF")
+        if (switchState) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(
+                    text = "얍얍",
+                    fontSize = 100.sp
+                )
+            }
         }
     }
 }
@@ -56,6 +95,6 @@ fun MyFirst() {
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyShowHideEx2()
     }
 }
