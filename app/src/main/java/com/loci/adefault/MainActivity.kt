86a130dbffa5d6a7b4237e8e1ad24e57ddfdacc8
kdbswo <kdbswo@gmail.com>
2023-09-09 04:09:51 +0900

    Navigation1 Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -3,19 +3,20 @@ package com.loci.adefault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.loci.adefault.ui.theme.DefaultTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,31 +24,91 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyNav()
+
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
-
-    Column {
-
-
+fun MyScreen1(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "화면1",
+            fontSize = 50.sp
+        )
+        Button(onClick = {
+            navHostController.navigate("myScreen2")
+        }) {
+            Text(
+                text = "2번화면으로 이동",
+                fontSize = 30.sp
+            )
+        }
     }
+}
 
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
+@Composable
+fun MyScreen2(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "화면2",
+            fontSize = 50.sp
+        )
+        Button(onClick = {
+            navHostController.navigate("myScreen3")
+        }) {
+            Text(
+                text = "3번 화면으로 이동",
+                fontSize = 30.sp
+            )
+        }
+    }
+}
 
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+@Composable
+fun MyScreen3(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "화면3",
+            fontSize = 50.sp
+        )
+        Button(onClick = {
+            navHostController.navigate("myScreen1")
+        }) {
+            Text(
+                text = "1번 화면으로 이동",
+                fontSize = 30.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun MyNav() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "myScreen1") {
+        composable("myScreen1") {
+            MyScreen1(navHostController = navController)
+        }
+        composable("myScreen2") {
+            MyScreen2(navHostController = navController)
+        }
+        composable("myScreen3") {
+            MyScreen3(navHostController = navController)
         }
     }
 }
@@ -56,6 +117,6 @@ fun MyFirst() {
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyNav()
     }
 }
