577cf5934b005afd7e02e66148f1e146dd8ef5c8
kdbswo <kdbswo@gmail.com>
2023-09-10 03:32:27 +0900

    Navigation2 Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -3,19 +3,27 @@ package com.loci.adefault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.loci.adefault.ui.theme.DefaultTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,39 +31,72 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyNav(navHostController = rememberNavController())
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
+fun MyNav(navHostController: NavHostController) {
 
-    Column {
-
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+    NavHost(navController = navHostController, startDestination = "myGridScreen") {
+        composable("myGridScreen") {
+            MyGridScreen(navHostController)
+        }
+        composable("myNumberScreen/{number}") { navBackStackEntry ->
+            MyNumberScreen(number = navBackStackEntry.arguments?.getString("number"))
         }
     }
+
+}
+
+@Composable
+fun MyGridScreen(navHostController: NavHostController) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        modifier = Modifier.padding(20.dp)
+    ) {
+        items(15) { number ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .border(1.dp, Color.Black)
+                    .clickable {
+                        navHostController.navigate("myNumberScreen/$number")
+                    }
+            ) {
+                Text(
+                    text = number.toString(),
+                    fontSize = 30.sp
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MyNumberScreen(number: String?) {
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = number.toString(),
+            fontSize = 70.sp
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyNav(navHostController = rememberNavController())
     }
 }
