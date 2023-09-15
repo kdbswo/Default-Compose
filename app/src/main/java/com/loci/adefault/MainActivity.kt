d4548bc06fd8fa95d3bab12af24f97d1ce4ccdba
kdbswo <kdbswo@gmail.com>
2023-09-15 20:03:49 +0900

    Canvas Ex

--- app/src/main/java/com/loci/adefault/MainActivity.kt
+++ app/src/main/java/com/loci/adefault/MainActivity.kt
@@ -3,19 +3,20 @@ package com.loci.adefault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.loci.adefault.ui.theme.DefaultTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,39 +24,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DefaultTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyFirst()
-                }
+                MyCanvas()
+
             }
         }
     }
 }
 
 @Composable
-fun MyFirst() {
+fun MyCanvas() {
 
-    Column {
-
-
-    }
-
-    Row {
-        Text(text = "Hello", fontSize = 50.sp)
-
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(20.dp)) {
-            Text(text = "asdf")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+            .background(Color.Green)
+    ) {
+        Canvas(modifier = Modifier.size(200.dp).align(Alignment.Center)) {
+            drawCircle(Color.Red, radius = size.minDimension / 2)
         }
     }
+
+
+//    Box(
+//        modifier = Modifier
+//            .width(100.dp)
+//            .height(200.dp)
+//            .background(Color.Green)
+//    ) {
+//        Canvas(modifier = Modifier.fillMaxSize()) {
+//            drawCircle(Color.Black, radius = size.minDimension / 3)
+//        }
+//    }
+
+
+//    Box(
+//        modifier = Modifier
+//            .size(50.dp)
+//            .background(Color.Green)
+//    ) {
+//        Canvas(modifier = Modifier.fillMaxSize()) {
+//            drawCircle(Color.Black, radius = size.minDimension / 10)
+//        }
+//    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DefaultTheme {
-        MyFirst()
+        MyCanvas()
     }
 }
